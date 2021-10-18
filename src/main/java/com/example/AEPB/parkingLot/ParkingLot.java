51533fb0eacad29bbb59b9d3c1796dfb73c158ae
nanxin <nanxin.xiao@thoughtworks.com>
2021-10-18 14:31:21 +0800

    [huijie&nanxin] test: add test that given empty fin when park car

--- src/main/java/com/example/AEPB/parkingLot/ParkingLot.java
+++ src/main/java/com/example/AEPB/parkingLot/ParkingLot.java
@@ -4,6 +4,7 @@ import com.example.AEPB.parkingLot.dto.ParkingTicket;
 import com.example.AEPB.parkingLot.dto.Vehicle;
 import com.example.AEPB.parkingLot.exception.CanNotGetTicketException;
 import lombok.*;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,12 +19,12 @@ public class ParkingLot {
     public static final int MAX_PARKING_VEHICLE_NUMBER = 50;
     private Map<ParkingTicket, Vehicle> parkingTicketAndVehicleMappings = new HashMap<>();
 
-    public Vehicle getVehicle(ParkingTicket parkingTicket){
+    public Vehicle pickUpCar(ParkingTicket parkingTicket){
         return parkingTicketAndVehicleMappings.remove(parkingTicket);
     }
 
-    public ParkingTicket getParkingTicket(Vehicle vehicle){
-        if (isNull(vehicle) || isNull(vehicle.getVin())) {
+    public ParkingTicket parkCar(Vehicle vehicle){
+        if (isNull(vehicle) || StringUtils.isEmpty(vehicle.getVin())) {
             throw new CanNotGetTicketException("can not get ticket when vehicle is null");
         }
         if (parkingTicketAndVehicleMappings.size() == MAX_PARKING_VEHICLE_NUMBER) {
--- src/test/java/com/example/AEPB/parkingLot/ParkingLotTest.java
+++ src/test/java/com/example/AEPB/parkingLot/ParkingLotTest.java
@@ -3,15 +3,24 @@ package com.example.AEPB.parkingLot;
 import com.example.AEPB.parkingLot.dto.ParkingTicket;
 import com.example.AEPB.parkingLot.dto.Vehicle;
 import com.example.AEPB.parkingLot.exception.CanNotGetTicketException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ParkingLotTest {
+    private ParkingLot parkingLot;
+
+    @BeforeEach
+    private void setup() {
+        parkingLot = new ParkingLot();
+    }
 
     /**
      * 测试策略：
@@ -20,11 +29,10 @@ class ParkingLotTest {
      * then: 拿到票根
      */
     @Test
-    void should_get_parking_ticket_success_when_put_vehicle_given_parking_lot_is_null_and_user_has_one_vehicle() {
+    void should_get_parking_ticket_success_when_parking_vehicle_given_parking_lot_is_null_and_user_has_one_vehicle() {
         Vehicle vehicle = new Vehicle("123");
-        ParkingLot parkingLot = new ParkingLot();
 
-        ParkingTicket parkingTicket = parkingLot.getParkingTicket(vehicle);
+        ParkingTicket parkingTicket = parkingLot.parkCar(vehicle);
 
         assertEquals(1, parkingLot.getParkingTicketAndVehicleMappings().size());
         assertEquals("123", parkingLot.getParkingTicketAndVehicleMappings().get(parkingTicket).getVin());
@@ -37,10 +45,9 @@ class ParkingLotTest {
      * then: 拿到票根
      */
     @Test
-    void should_get_parking_ticket_success_when_put_vehicle_given_parking_lot_has_49_vehicle_and_user_has_ont_vehicle() {
-        ParkingLot parkingLot = new ParkingLot();
+    void should_get_parking_ticket_success_when_parking_vehicle_given_parking_lot_has_49_vehicle_and_user_has_one_vehicle() {
         Map<ParkingTicket, Vehicle> parkingTicketAndVehicleMappings = new HashMap<>();
-        for (int i=0; i < 49; i++) {
+        for (int i = 0; i < 49; i++) {
             Vehicle vehicle = new Vehicle("ori" + i);
             ParkingTicket parkingTicket = new ParkingTicket(UUID.randomUUID().toString());
             parkingTicketAndVehicleMappings.put(parkingTicket, vehicle);
@@ -48,7 +55,7 @@ class ParkingLotTest {
         parkingLot.setParkingTicketAndVehicleMappings(parkingTicketAndVehicleMappings);
         Vehicle vehicle = new Vehicle("123");
 
-        ParkingTicket parkingTicket = parkingLot.getParkingTicket(vehicle);
+        ParkingTicket parkingTicket = parkingLot.parkCar(vehicle);
 
         assertEquals(50, parkingLot.getParkingTicketAndVehicleMappings().size());
         assertEquals("123", parkingLot.getParkingTicketAndVehicleMappings().get(parkingTicket).getVin());
@@ -62,10 +69,9 @@ class ParkingLotTest {
      * then: 拿不到票根
      */
     @Test
-    void should_get_parking_ticket_failed_when_put_vehicle_given_parking_lot_has_50_vehicle_and_user_has_one_vehicle() {
-        ParkingLot parkingLot = new ParkingLot();
+    void should_get_parking_ticket_failed_when_parking_vehicle_given_parking_lot_has_50_vehicle_and_user_has_one_vehicle() {
         Map<ParkingTicket, Vehicle> parkingTicketAndVehicleMappings = new HashMap<>();
-        for (int i=0; i < 50; i++) {
+        for (int i = 0; i < 50; i++) {
             Vehicle vehicle = new Vehicle("ori" + i);
             ParkingTicket parkingTicket = new ParkingTicket(UUID.randomUUID().toString());
             parkingTicketAndVehicleMappings.put(parkingTicket, vehicle);
@@ -73,20 +79,19 @@ class ParkingLotTest {
         parkingLot.setParkingTicketAndVehicleMappings(parkingTicketAndVehicleMappings);
         Vehicle vehicle = new Vehicle("123");
 
-        assertNull(parkingLot.getParkingTicket(vehicle));
+        assertNull(parkingLot.parkCar(vehicle));
     }
 
     /**
      * 测试策略：
-     * given: 停车场内有停车2辆，用户没有车，想占用停车厂位置
+     * given: 停车场内有停车2辆，用户没有车，想占用停车场位置
      * when: 存车
      * then: 抛出CanNotGetTicketException异常
      */
     @Test
-    void should_get_can_not_get_ticket_exception_when_put_vehicle_given_parking_lot_has_2_vehicle_and_user_has_no_vehicle() {
-        ParkingLot parkingLot = new ParkingLot();
+    void should_get_can_not_get_ticket_exception_when_parking_vehicle_given_parking_lot_has_2_vehicle_and_user_has_no_vehicle() {
         Map<ParkingTicket, Vehicle> parkingTicketAndVehicleMappings = new HashMap<>();
-        for (int i=0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) {
             Vehicle vehicle = new Vehicle("ori" + i);
             ParkingTicket parkingTicket = new ParkingTicket(UUID.randomUUID().toString());
             parkingTicketAndVehicleMappings.put(parkingTicket, vehicle);
@@ -94,21 +99,20 @@ class ParkingLotTest {
         parkingLot.setParkingTicketAndVehicleMappings(parkingTicketAndVehicleMappings);
         Vehicle vehicle = null;
 
-        assertThrows(CanNotGetTicketException.class, () -> parkingLot.getParkingTicket(vehicle));
+        assertThrows(CanNotGetTicketException.class, () -> parkingLot.parkCar(vehicle));
     }
 
 
     /**
      * 测试策略：
-     * given: 停车场内有停车2辆，用户车没有vin号，想占用停车厂位置
+     * given: 停车场内有停车2辆，用户车没有vin号，想占用停车场位置
      * when: 存车
      * then: 抛出CanNotGetTicketException异常
      */
     @Test
-    void should_get_can_not_get_ticket_exception_when_put_vehicle_given_parking_lot_has_2_vehicle_and_user_vehicle_has_no_vin() {
-        ParkingLot parkingLot = new ParkingLot();
+    void should_get_can_not_get_ticket_exception_when_parking_vehicle_given_parking_lot_has_2_vehicle_and_user_vehicle_has_no_vin() {
         Map<ParkingTicket, Vehicle> parkingTicketAndVehicleMappings = new HashMap<>();
-        for (int i=0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) {
             Vehicle vehicle = new Vehicle("ori" + i);
             ParkingTicket parkingTicket = new ParkingTicket(UUID.randomUUID().toString());
             parkingTicketAndVehicleMappings.put(parkingTicket, vehicle);
@@ -116,9 +120,29 @@ class ParkingLotTest {
         parkingLot.setParkingTicketAndVehicleMappings(parkingTicketAndVehicleMappings);
         Vehicle vehicle = new Vehicle();
 
-        assertThrows(CanNotGetTicketException.class, () -> parkingLot.getParkingTicket(vehicle));
+        assertThrows(CanNotGetTicketException.class, () -> parkingLot.parkCar(vehicle));
     }
 
+
+    /**
+     * 测试策略：
+     * given: 停车场内有停车2辆，用户的车vin号为空，想占用停车场位置
+     * when: 存车
+     * then: 抛出CanNotGetTicketException异常
+     */
+    @Test
+    void should_get_can_not_get_ticket_exception_when_parking_vehicle_given_parking_lot_has_2_vehicle_and_user_vehicle_vin_is_empty() {
+        Map<ParkingTicket, Vehicle> parkingTicketAndVehicleMappings = new HashMap<>();
+        for (int i = 0; i < 2; i++) {
+            Vehicle vehicle = new Vehicle("ori" + i);
+            ParkingTicket parkingTicket = new ParkingTicket(UUID.randomUUID().toString());
+            parkingTicketAndVehicleMappings.put(parkingTicket, vehicle);
+        }
+        parkingLot.setParkingTicketAndVehicleMappings(parkingTicketAndVehicleMappings);
+        Vehicle vehicle = new Vehicle("");
+
+        assertThrows(CanNotGetTicketException.class, () -> parkingLot.parkCar(vehicle));
+    }
 
     /**
      * 测试策略：
@@ -127,14 +151,14 @@ class ParkingLotTest {
      * then: 用户取到自己的车
      */
     @Test
-    void should_get_vehicle_by_parking_ticket_success_when_get_vehicle_given_ticket_is_legal() {
+    void should_get_vehicle_by_parking_ticket_success_when_pick_up_vehicle_given_ticket_is_legal() {
         Map<ParkingTicket, Vehicle> parkingTicketAndVehicleMappings = new HashMap<>();
         ParkingTicket parkingTicket = new ParkingTicket("ticketNumber");
         Vehicle vehicle = new Vehicle("vin001");
         parkingTicketAndVehicleMappings.put(parkingTicket, vehicle);
-        ParkingLot parkingLot = new ParkingLot(parkingTicketAndVehicleMappings);
+        parkingLot.setParkingTicketAndVehicleMappings(parkingTicketAndVehicleMappings);
 
-        Vehicle vehicleGetFromLot = parkingLot.getVehicle(parkingTicket);
+        Vehicle vehicleGetFromLot = parkingLot.pickUpCar(parkingTicket);
 
         assertEquals("vin001", vehicleGetFromLot.getVin());
     }
@@ -146,11 +170,10 @@ class ParkingLotTest {
      * then: 取不到车
      */
     @Test
-    void should_get_vehicle_by_parking_ticket_failed_when_get_vehicle_given_ticket_has_been_used() {
+    void should_get_vehicle_by_parking_ticket_failed_when_pick_up_vehicle_given_ticket_has_been_used() {
         ParkingTicket parkingTicket = new ParkingTicket("ticketNumber");
-        ParkingLot parkingLot = new ParkingLot();
 
-        Vehicle vehicleGetFromLot = parkingLot.getVehicle(parkingTicket);
+        Vehicle vehicleGetFromLot = parkingLot.pickUpCar(parkingTicket);
 
         assertEquals(null, vehicleGetFromLot);
     }
@@ -162,10 +185,10 @@ class ParkingLotTest {
      * then: 取不到车
      */
     @Test
-    void should_get_vehicle_by_parking_ticket_failed_when_get_vehicle_given_ticket_is_null() {
+    void should_get_vehicle_by_parking_ticket_failed_when_pick_up_vehicle_given_ticket_is_null() {
         ParkingLot parkingLot = new ParkingLot();
 
-        Vehicle vehicleGetFromLot = parkingLot.getVehicle(null);
+        Vehicle vehicleGetFromLot = parkingLot.pickUpCar(null);
 
         assertEquals(null, vehicleGetFromLot);
     }
@@ -177,10 +200,9 @@ class ParkingLotTest {
      * then: 取不到车
      */
     @Test
-    void should_get_vehicle_by_parking_ticket_failed_when_get_vehicle_given_ticket_ticket_number_is_null() {
-        ParkingLot parkingLot = new ParkingLot();
+    void should_get_vehicle_by_parking_ticket_failed_when_pick_up_vehicle_given_ticket_ticket_number_is_null() {
 
-        Vehicle vehicleGetFromLot = parkingLot.getVehicle(new ParkingTicket());
+        Vehicle vehicleGetFromLot = parkingLot.pickUpCar(new ParkingTicket());
 
         assertEquals(null, vehicleGetFromLot);
     }
